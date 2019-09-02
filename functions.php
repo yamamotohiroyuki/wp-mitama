bc3d4b79b38c5575953c514fe1fe3f7565f7006a
山本 博之 <bigupsanche@gmail.com>
2019-09-02 19:58:33 +0900

    プラグインチェッカー廃止

--- functions.php
+++ functions.php
@@ -1,16 +1,4 @@
 <?php
-// update checker
-/*
-require_once get_stylesheet_directory_uri( 'function_inc/update-checker.php' );
-$update_json_path = 'https://raw.githubusercontent.com/yamamotohiroyuki/wp-mitama/master/theme.json';
-$thema_name = 'mitama';
-require get_template_directory() . '/plugin-update-checker/plugin-update-checker.php';
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-  $update_json_path, // (A)
-    __FILE__,
-    $thema_name // (B)
-);*/
-
 // 基本的な関数
 require_once get_parent_theme_file_path( 'function_inc/func-basic.php' );
 // スタイルとスクリプトの読み込み
